b5a6750f03d7ad961f78fa780b783fd4aa55613a
eliza <samoylovaeliza@gmail.com>
2019-10-23 23:56:27 +0300

    добавила контент

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
   $(document).ready(function()
   {
-    $('.owl-carousel').owlCarousel({
+    $('.owl-one').owlCarousel({
 
       items:1,
       loop:true,
@@ -21,6 +21,49 @@
       animateIn: 'fadeIn',
               animateOut: 'fadeOut'
        
+    });
+
+
+    $('.owl-two').owlCarousel({
+
+      items:6,
+      loop:true,
+      margin:20,
+      nav:true,
+      navText:[
+            '<i class="fa fa-angle-left fa-4x"></i>',
+            '<i class="fa fa-angle-right fa-4x"></i>'
+          ],
+      autoplay:true,
+      smartSpeed:1000, //Время движения слайда
+              autoplayTimeout:3000,
+              autoplayHoverPause:true,
+              dots:false,
+              pagination:true,
+
+              responsive:{
+                  0:{
+                      items:1,
+                      nav:true,
+                      loop:true
+                  },
+                  400:{
+                      items:3,
+                      nav:true,
+                      loop:true
+                  },
+                  600:{
+                      items:4,
+                      nav:true,
+                      loop:true
+                  },
+                  
+                  1000:{
+                      items:6,
+                      nav:true,
+                      loop:true
+                  }
+              }
           
 
     });
